12e3bf75ed40eee1c0b9535baa697661c88d41be
homapm <homa.pourmohammadi@gmail.com'git config --global user.email homa.pourmohammadi@gmail.com3.git config --global user.email "homa.pourmohammadi@gmail.com>
2018-12-25 16:59:47 +0330

    Global exceptionhandling for .net core web API files

---
+++ GlobalExceptionHandling/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GlobalExceptionHandling.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context )
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError; 
+
+            if (exception is InvalidOperationException) code = HttpStatusCode.Conflict;
+            //if (exception is SomeNotFoundException) code = HttpStatusCode.NotFound;
+            //else if (exception is SomeUnauthorizedException) code = HttpStatusCode.Unauthorized;
+            //else if (exception is SomeException) code = HttpStatusCode.BadRequest;
+
+            var result = JsonConvert.SerializeObject(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+
+    }
+}
